813da1fa6f11d9d5b3cc9bf2ec7678148b9d2fab
Dominik Prokop <dominik.prokop@grafana.com>
2022-01-13 08:44:00 +0100

    Make SQLEditor compile for  v < 8

--- src/sql-editor/utils/debugger.ts
+++ src/sql-editor/utils/debugger.ts
@@ -1,5 +1,12 @@
 import { attachDebugger, createLogger } from "@grafana/ui";
 
-export const sqlEditorLogger = createLogger('SQLEditor');
-export const sqlEditorLog = sqlEditorLogger.logger;
-attachDebugger('sqleditor', undefined, sqlEditorLogger);
+let sqlEditorLogger = { logger : () => {}}
+let sqlEditorLog = () => {}
+
+if (attachDebugger && createLogger) {
+     sqlEditorLogger = createLogger('SQLEditor');
+     sqlEditorLog = sqlEditorLogger.logger;
+     attachDebugger('sqleditor', undefined, sqlEditorLogger as any);
+}
+
+export { sqlEditorLog, sqlEditorLogger };
