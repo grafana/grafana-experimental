7bbf7cce10f0c60ad13a52a8907ad5f422f78882
Andriy Urbanas <andriy.urbanas@grafana.com>
2023-05-12 10:26:02 +0200

    feat: Minor changes to DataSourceDescription component

--- src/ConfigEditor/DataSourceDescription.tsx
+++ src/ConfigEditor/DataSourceDescription.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import { css } from '@emotion/css';
+import { cx, css } from '@emotion/css';
 import { useTheme2 } from '@grafana/ui';
 
 type Props = {
   dataSourceName: string;
   docsLink: string;
   hasRequiredFields?: boolean;
+  className?: string;
 };
 
-export const DataSourceDescription = ({ dataSourceName, docsLink, hasRequiredFields = true }: Props) => {
+export const DataSourceDescription = ({ dataSourceName, docsLink, hasRequiredFields = true, className }: Props) => {
   const theme = useTheme2();
 
   const styles = {
     container: css({
-      margin: theme.spacing(4, 0),
+      p: {
+        margin: 0,
+      },
+      'p + p': {
+        marginTop: theme.spacing(2),
+      },
     }),
     text: css({
       ...theme.typography.body,
@@ -32,11 +38,10 @@ export const DataSourceDescription = ({ dataSourceName, docsLink, hasRequiredFie
   };
 
   return (
-    <div className={styles.container}>
+    <div className={cx(styles.container, className)}>
       <p className={styles.text}>
-        Before you can use the {dataSourceName} data source, you must configure it below or in the config file.
-        <br />
-        For detailed instructions,{' '}
+        Before you can use the {dataSourceName} data source, you must configure it below or in the config file. For
+        detailed instructions,{' '}
         <a href={docsLink} target="_blank" rel="noreferrer">
           view the documentation
         </a>
