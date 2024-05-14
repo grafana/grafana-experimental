f75c0604f5bb2a490908c29de9e38132689c8559
Ashley Harrison <ashley.harrison@grafana.com>
2024-05-14 09:30:40 +0100

    Revert "remove defaultProps and update to newer grafana packages"
    This reverts commit 71495823ee64ce2dc5fbcf420957333d87ebe768.

--- src/QueryEditor/Space.tsx
+++ src/QueryEditor/Space.tsx
@@ -1,5 +1,5 @@
 import { css, cx } from '@emotion/css';
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
 import { useStyles2 } from '@grafana/ui';
@@ -13,26 +13,28 @@ export interface SpaceProps {
 /**
  * @deprecated use the Space component from @grafana/ui instead. Available starting from @grafana/ui@10.4.0
  */
-export const Space = ({
-  v = 0,
-  h = 0,
-  layout = 'block',
-}: SpaceProps) => {
-  const styles = useStyles2(getStyles, v, h, layout);
+export const Space = (props: SpaceProps) => {
+  const styles = useStyles2(useCallback((theme) => getStyles(theme, props), [props]));
 
   return <span className={cx(styles.wrapper)} />;
 };
 
-const getStyles = (theme: GrafanaTheme2, v: SpaceProps['v'], h: SpaceProps['v'], layout: SpaceProps['layout']) => ({
+Space.defaultProps = {
+  v: 0,
+  h: 0,
+  layout: 'block',
+};
+
+const getStyles = (theme: GrafanaTheme2, props: SpaceProps) => ({
   wrapper: css([
     {
-      paddingRight: theme.spacing(h ?? 0),
-      paddingBottom: theme.spacing(v ?? 0),
+      paddingRight: theme.spacing(props.h ?? 0),
+      paddingBottom: theme.spacing(props.v ?? 0),
     },
-    layout === 'inline' && {
+    props.layout === 'inline' && {
       display: 'inline-block',
     },
-    layout === 'block' && {
+    props.layout === 'block' && {
       display: 'block',
     },
   ]),
